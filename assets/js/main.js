d6b6a38b518b45a58ef43d42f3e65646c3affb2b
rochellelatayan <rochelle.latayan@codeandtheory.com>
2015-11-15 12:53:22 +0800

    top nav responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,40 @@ $(document).ready(function() {
 		}
 	});
 	
-	//burger responsive
+	//top link responsive
+	var topMore = $('.moreNavContents');
+	var tmHeight = $('.moreNavContents')[0].scrollHeight;
 	
+	//nav hover
+	$('.moreNav').click(function() {
+		
+		if(!$(this).hasClass('show')) {
+			$(this).addClass('show');
+			$(topMore).addClass('show visible');
+			$(topMore).css('height', tmHeight);
+		} else {
+			$(this).removeClass('show');
+			$(topMore).removeClass('show visible');
+			$(topMore).css('height', '0px');
+		}
+	});
+	
+	//more responsive
+	var ddMore = $('.more ul');
+	var moreHeight = $('.more ul')[0].scrollHeight;
+	var totalHeight = moreHeight + tmHeight;
+	//nav hover
+	$('.more').click(function() {
+		if(!$(this).hasClass('show')) {
+			$(this).addClass('show');
+			$(ddMore).css('height', moreHeight); //dynamic height
+			$(topMore).css('height', totalHeight);
+		} else {
+			$(this).removeClass('show');
+			$(ddMore).css('height', '0');
+			$(topMore).css('height', tmHeight);
+		}
+	});
 	
 	
 });
