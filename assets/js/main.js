0951a193e1c8f23cae5f17bd92d262aea7f17d9d
rochellelatayan <rochelle.latayan@codeandtheory.com>
2015-11-16 20:15:19 +0800

    1366px

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,12 @@ $(document).ready(function() {
 	
 	//dropdown var and height
 	var dd = $('.dropdown ul');
-	var wholeHeight = $('.dropdown ul')[0].scrollHeight;
 	
 	//nav hover
 	$('.dropdown').hover(function() {
 		$(this).addClass('active');
-		$(dd).css('height', wholeHeight); //dynamic height
 	}, function() {
 		$(this).removeClass('active');
-		$(dd).css('height', '0px');
 	});
 	
 	//search
